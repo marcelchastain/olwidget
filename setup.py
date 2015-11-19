82e8be08afc66b0bcba993181169d67589bad29c
Max Arnold <arnold.maxim@gmail.com>
2015-11-19 13:07:11 +0700

    change static files layout to fix package installation, bump version to 0.61.2

--- setup.py
+++ setup.py
@@ -7,12 +7,7 @@ from distutils.core import setup
 root = os.path.abspath(os.path.dirname(__file__))
 os.chdir(root)
 
-VERSION = '0.61.1'
-
-# Make data go to the right place.
-# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
+VERSION = '0.61.2'
 
 setup(name='django-olwidget',
     version=VERSION,
