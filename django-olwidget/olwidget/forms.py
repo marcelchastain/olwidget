af6b06cf31554f83d20da28302ac74fa342150b3
Max Arnold <arnold.maxim@gmail.com>
2016-06-16 11:47:39 +0700

    attempt to fix https://github.com/yourcelf/olwidget/issues/124

--- django-olwidget/olwidget/forms.py
+++ django-olwidget/olwidget/forms.py
@@ -1,11 +1,46 @@
 from django import forms
+from django.forms.fields import Field
 from django.contrib.gis.forms.fields import GeometryField
+from collections import OrderedDict
+import six
 
 from olwidget.widgets import Map, BaseVectorLayer, EditableLayer
 from olwidget.fields import MapField
 from olwidget import utils
 
 __all__ = ('MapModelForm', )
+
+def get_declared_fields(bases, attrs, with_base_fields=True):
+    """
+    Create a list of form field instances from the passed in 'attrs', plus any
+    similar fields on the base classes (in 'bases'). This is used by both the
+    Form and ModelForm metaclasses.
+
+    If 'with_base_fields' is True, all fields from the bases are used.
+    Otherwise, only fields in the 'declared_fields' attribute on the bases are
+    used. The distinction is useful in ModelForm subclassing.
+    Also integrates any additional media definitions.
+    """
+    fields = [
+        (field_name, attrs.pop(field_name))
+        for field_name, obj in list(six.iteritems(attrs)) if isinstance(obj, Field)
+    ]
+    fields.sort(key=lambda x: x[1].creation_counter)
+
+    # If this class is subclassing another Form, add that Form's fields.
+    # Note that we loop over the bases in *reverse*. This is necessary in
+    # order to preserve the correct order of fields.
+    if with_base_fields:
+        for base in bases[::-1]:
+            if hasattr(base, 'base_fields'):
+                fields = list(six.iteritems(base.base_fields)) + fields
+    else:
+        for base in bases[::-1]:
+            if hasattr(base, 'declared_fields'):
+                fields = list(six.iteritems(base.declared_fields)) + fields
+
+    return OrderedDict(fields)
+
 
 class BaseMapModelForm(forms.models.BaseModelForm):
     """
@@ -54,7 +89,7 @@ class MapModelFormMetaclass(type):
         except NameError:
             # We are defining MapModelForm itself.
             parents = None
-        declared_fields = forms.models.get_declared_fields(bases, attrs, False)
+        declared_fields = get_declared_fields(bases, attrs, False)
         new_class = super(MapModelFormMetaclass, mcs).__new__(mcs, name, bases,
                 attrs)
         if not parents:
